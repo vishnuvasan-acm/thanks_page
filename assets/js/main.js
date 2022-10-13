0b9c043d47440220f0a95199244310bc50b2d8c1
Dexters <113793678+Dexters-IN@users.noreply.github.com>
2022-10-13 22:21:37 +0530

    CountDown Timer Fixed

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,7 +14,7 @@
             onePage: false,
         });
         $(".counter").counterUp({ delay: 10, time: 1000 });
-        $(".countdown").downCount({ date: "12/15/2021 12:00:00", offset: +6 }, function () {
+        $(".countdown").downCount({ date: "10/21/2022 12:00:00", offset: +6 }, function () {
             console.log("Countdown done!");
         });
         $(".popup-link").magnificPopup({
